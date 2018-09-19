a9c76d2763729d1e238f18fc11ef9baf0af313fd
Dannymaina <dannymaina@gmail.com>
2018-09-19 15:22:38 +0300

    added vue user registration

---
+++ client/src/store/authentication.js
@@ -0,0 +1,44 @@
+import router from '../router'
+import HTTP from '../http'
+export default {
+    namespaced: true,
+    state: {
+        registerEmail: "hello",
+        registerPassword: "world",
+        registerError: null,
+        token: null
+    },
+    actions: {
+        register({commit, state}){
+            commit('setRegisterError', null)            
+            HTTP().post('auth/register', {
+                email: state.registerEmail,
+                password: state.registerPassword,
+            }).then(({data}) => {
+                commit('setToken', data.token)
+                router.push('/')
+            }).catch(() => {
+                commit('setRegisterError', 'Error while registering user')
+            })
+        }
+    },
+    getters: {
+        isLoggedIn(state){
+            return !!state.token
+        }
+    },
+    mutations:{
+        setToken(state, token){
+            state.token = token
+        },
+        setRegisterError(state, error){
+            state.registerError = error
+        },
+        setRegisterEmail(state, email){
+            state.registerEmail = email
+        },
+        setRegisterPassword(state, password){
+            state.registerPassword = password
+        },
+    }
+}
